d135e5b9084e15e439a3b2b068a36c14456874b4
LGocool <67392650+LGocool@users.noreply.github.com>
2023-02-08 23:19:22 -0500

    Passed all nine test in the arrow functions lab

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (a,b) => {
+    let num1 = a;
+    let num2 = b;
+    return a/b ;
+}
+
+const square = (num1) => {
+    let x = num1;
+    return x**x;
+}
+
+const add = (c,d) => {
+    let num3 = c;
+    let num4 = d;
+    return num3 + num4;
+}
